0c3ac229e0c854b55019a56c07a5f0bc28b16af5
Ivan <ikalajzi1@pmfst.hr>
2022-06-19 13:50:23 +0200

    ispravak zadatka

--- Vjezbe11/1.py
+++ Vjezbe11/1.py
@@ -5,12 +5,13 @@ import gravity as grt
 p1 = grt.SunEarth()
 def vektor(x, y):
     return np.array([x, y])
-p1.set_initial_conditions(vektor(0, 0), vektor(0, 29783), vektor(1.486*((10)**11), 0), vektor(0, 0))
-x, y, x1, y1 = p1.Euler_method(365.0)
-fig, axs = plt.subplots()
-
+p1.set_initial_conditions(vektor(0, 0), vektor(0, 29783), np.array([1.486*10**(11), 0]), np.array([0.1, 0.1]))
+x, y, x1, y1 = p1.Euler_method()
+fig, axs = plt.subplots(figsize=(6,6))
 axs.plot(x, y)
 axs.plot(x1, y1)
-axs.set_title("sun-earth")
-
+axs.legend(["Zemlja", "Sunce"])
+axs.set_title("Zemlja-Sunce")
+axs.set_xlabel("x")
+axs.set_ylabel("y")
 plt.show()
--- Vjezbe11/gravity.py
+++ Vjezbe11/gravity.py
@@ -9,41 +9,44 @@ class SunEarth:
         self.lista_Sx = []
         self.lista_Sy = []
         self.lista_t = []
-        self.dt = 1.1
+        self.dt = 100
         self.G = 6.67408*(10**(-11))
         self.Ms =  1.989*((10)**30)
         self.Mz = 5.9742*((10)**24)
+        self.tt = 365.24*24*3600
 
-    def set_initial_conditions(self, Vs, Vz, Rs, Rz):
-        self.lista_Zx.append(0)
+    def set_initial_conditions(self, Vs, Vz, Rz, Rs):
+        self.lista_Zx.append(1.486*10**(11))
         self.lista_Zy.append(0)
         self.lista_Sx.append(0)
         self.lista_Sy.append(0)
         self.lista_t.append(0)
-        self.As = np.array([0, 0, 0])
-        self.Az = np.array([0, 0, 0])
+        self.As = np.array([0, 0])
+        self.Az = np.array([0, 0])
         self.Vs = Vs
         self.Vz = Vz
         self.Rs = Rs
         self.Rz = Rz
         
     def __move(self):
-        self.Az = (-self.G*(self.Ms*(self.Rz-self.Rs)))/(abs(self.Rz-self.Rs))**3
+        dx = (self.Rz - self.Rs)**2
+        self.Az = (-self.G*(self.Ms*(self.Rz-self.Rs)))/((math.sqrt(dx[0]+dx[1])))**3
         self.Vz = self.Vz+self.Az*self.dt
-        self.lista_Zx.append(self.lista_Zx[-1]+self.Vz[0]*self.dt)
-        self.lista_Zy.append(self.lista_Zy[-1]+self.Vz[1]*self.dt)
+        self.Rz = self.Rz + self.Vz*self.dt
+        self.lista_Zx.append(self.Rz[0])
+        self.lista_Zy.append(self.Rz[1])
 
-        self.As = (-self.G*(self.Mz*(self.Rs-self.Rz)))/(abs(self.Rs-self.Rz))**3
+        dy = (self.Rs - self.Rz)**2
+        self.As = (-self.G*(self.Mz*(self.Rs-self.Rz)))/((math.sqrt(dy[0]+dy[1])))**3
         self.Vs = self.Vs+self.As*self.dt
-        self.lista_Sx.append(self.lista_Sx[-1]+self.Vs[0]*self.dt)
-        self.lista_Sy.append(self.lista_Sy[-1]+self.Vs[1]*self.dt)
+        self.Rs = self.Rs + self.Vs*self.dt
+        self.lista_Sx.append(self.Rs[0])
+        self.lista_Sy.append(self.Rs[1])
+    
+        self.lista_t.append(self.lista_t[-1]+self.dt)
 
-        self.Rz = ([self.lista_Zx[-1], self.lista_Zy[-1]])
-        self.Rs = ([self.lista_Sx[-1], self.lista_Sy[-1]])
-
-    def Euler_method(self, t):
-        n = int(t/self.dt) 
-        for i in range(n):
+    def Euler_method(self):
+        while self.lista_t[-1] <= self.tt:
             self.__move()  
         return self.lista_Zx, self.lista_Zy, self.lista_Sx, self.lista_Sy        
     
